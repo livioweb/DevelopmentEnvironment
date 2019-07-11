883689bca6172f39a925a692dc149945d5b34cc4
Livio Lopes <liviorodrigueslopes@gmail.com>
2019-07-11 10:24:15 -0300

    Inslando mais ferramantas

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+
+#install version code GIT
+sudo apt install git
+
+#install textEditCode VIM
+sudo apt install vim
+
+#install term yakuake 
+sudo apt install libkf5globalaccel-bin
+sudo apt install yakuake
+
+#Install IDE VSCODE
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -19,3 +38,8 @@ su - $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+#Install DK - DocKer Manager
+wget https://github.com/livioweb/dk_docker_manager/blob/master/dk
+sudo mv dk /bin/dk
