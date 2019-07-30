aa3075f59ec2d3b8d297a561c77b302bac4729ad
Livio Lopes <liviorodrigueslopes@gmail.com>
2019-07-29 22:36:26 -0300

    Correcao na linha docker-composer permissao

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -47,7 +47,6 @@ sudo apt -y  install code
 
 #Install composer
 sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-sudo chmod +x /usr/local/bin/docker-compose
 
 #Install docker-composer
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
