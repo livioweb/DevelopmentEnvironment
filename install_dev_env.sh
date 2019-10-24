b2517e5821805de3a9663f7c3a67f1daca145ccc
KAOSTECK <liviorodrigueslopes@gmail.com>
2019-10-24 17:10:57 -0300

    Update install_dev_env.sh

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -61,13 +61,15 @@ sudo apt-get install -y  docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+zsh
+
 # https://docs.docker.com/install/linux/linux-postinstall/
 sudo groupadd docker
 sudo usermod -aG docker $USER
 su - $USER  
 
-sudo apt-get install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-zsh
+
 
 
