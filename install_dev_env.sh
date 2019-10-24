e57d9cb9d1317d18b00b51ac206d8dd9c8fe2bbf
KAOSTECK <liviorodrigueslopes@gmail.com>
2019-10-24 16:54:41 -0300

    Update install_dev_env.sh

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Livio Rodrigues
 #Versao 1.0.1
+
 sudo apt-get update
 
 #install php
@@ -48,9 +49,6 @@ sudo apt-get -y  install code
 #Install composer
 sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-#Install docker-composer
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -58,6 +56,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y  docker-ce
 
+#Install docker-composer
 # https://docs.docker.com/compose/install/
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
